2290ef2342ea391cfe6766e6c8ccecda20b67435
tyuownu <tyuownu@gmail.com>
2015-12-09 10:34:43 +0800

    temp commit.

---
+++ include/rgbd_image.h
@@ -0,0 +1,9 @@
+#ifndef _RGBD_IMAGE_H_
+#define _RGBD_IMAGE_H_
+
+#include <opencv2/opencv.hpp>
+#include <Eigen/Geometry>
+#include <Eigen/StdVector>
+#include <boost/smart_ptr.hpp>
+
+#include <intrinsic_matrix.h>
